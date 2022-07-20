887fb5b5772f58bb2ab28369cdaa255706a1e0af
jake-p <jake-p@users.noreply.github.com>
2022-07-19 20:05:51 -0400

    Create GridObject.cs

---
+++ GridObject.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridObject : MonoBehaviour
+{
+    [SerializeField] Grid targetGrid;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        Vector2Int positionOnGrid = targetGrid.GetGridPosition(transform.position);
+        targetGrid.PlaceObject(positionOnGrid, this);
+    }
+}
